baee7681a9f77103a68ca27c6838552a21349f01
alexandr <vhalexemelkin@gmail.c0m>
2022-09-11 12:06:29 +0200

    bonus

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-print("Ноль в качестве знака завершит работу")
-while True:
-    s = input("выбери знак +,-,*,/: ")
-    if s == '0':
-        break
-    if s in ('+', '-', '*', '/'):
-        x = float(input("x="))
-        y = float(input("y="))
-        if s == '+':
-            print(x+y)
-        elif s == '-':
-            print(x-y)
-        elif s == '*':
-            print(x*y)
-        elif s == '/':
-                print(x/y)
-    else:
-        print("неверный знак")
+print("kalkulator")
+a = int(input("1:"))
+b = int(input("2:"))
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b}")
