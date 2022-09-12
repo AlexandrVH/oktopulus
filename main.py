64abe130f76c202c92247f7421e7e5d4d8774ea3
alexandr <vhalexemelkin@gmail.c0m>
2022-09-12 19:37:56 +0200

    privetstvie+ mini bonus for my teacher(there is a secret in the code)

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 
-print("Helo mister DarkValkier")
-a = int(input("1:"))
-b = int(input("2:"))
-print(f"{a} + {b} = {a + b}")
-print(f"{a} - {b} = {a - b}")
-print(f"{a} * {b} = {a * b}")
-print(f"{a} / {b} = {a / b}")
+print("Helo mister DarkValkier, this is my upgreded calculator")
+print("0--finish this program")
+while True:
+    s = input("your choice (+,-,*,/): ")
+    if s == '0':
+        break
+    if s == 'B29162':
+        print("https://youtu.be/G1IbRujko-A")
+    if s in ('+', '-', '*', '/'):
+        x = float(input("x="))
+        y = float(input("y="))
+        if s == '+':
+            print("%.2f" % (x+y))
+        elif s == '-':
+            print("%.2f" % (x-y))
+        elif s == '*':
+            print("%.2f" % (x*y))
+        elif s == '/':
+            if y != 0:
+                print("%.2f" % (x/y))
+            else:
+                print("eror B29162")
